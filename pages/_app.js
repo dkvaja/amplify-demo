0a83143f3155a9c5d03c54ce9606cfba14fd6be9
dkvaja <vajadushyant143@gmail.com>
2022-06-06 17:03:26 +0530

    First Loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import React from "react";
+import Head from "next/Head";
+import "../styles/globals.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const theme = createTheme();
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Amplify Demo - Home</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
